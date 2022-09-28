bc7f1fff27dd86e04af3225202691a2bc68ff291
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-09-27 21:50:22 -0400

    Adding code for insertion sort

--- Algorithms/Sorting/Insertion Sort/insertion-sort.py
+++ Algorithms/Sorting/Insertion Sort/insertion-sort.py
@@ -1,8 +1,16 @@
 def insertion_sort(arr):
-  
+  for i in range(1, len(arr)):
+    curr = arr[i]
+    j = i
+    while (j > 0) and (arr[j - 1] > curr):
+      arr[j] = arr[j - 1]
+      j -= 1 
+    arr[j] = curr  
 
-sorted = insertion_sort([5, 9, 2, 6])
+  return arr
+  
+sorted = insertion_sort([5, 9, 2, 6, 20, 4.3, -2, 14])
 print(sorted)
 
-# Time Complexity: O()
+# Time Complexity: O(n^2)
 # Space Complexity: O(1)
